0176d90cf2fc7f6ba7a86f49f7fc68998d7aba85
Гуров Георгий <magnezia98@mail.ru>
2019-11-23 19:51:39 +0300

    Fix YML error

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,11 @@ module.exports = {
   entry: `./src/main.js`,
   output: {
     filename: `bundle.js`,
+    // eslint-disable-next-line
     path: path.join(__dirname, `public`),
   },
   devServer: {
+    // eslint-disable-next-line
     contentBase: path.join(__dirname, `public`),
     watchContentBase: true,
     port: 8081,
